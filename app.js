b2d152f56a174a0ad1d9818115b71ef9802e1b13
Kathy Coffee <klcoffee8807@gmail.com>
2021-10-28 10:57:49 -0400

    Added Event Listener for the Clipboard button.  Initial version of app.js file complete.  Initial version of project complete.

--- app.js
+++ app.js
@@ -13,17 +13,21 @@ function getRandomLower(){
 // Returning a random letter using a random index in the "letters" string 
     return letters[randomIndex(letters)];
 }
+// Example of the getRandomLower Function
+console.log(getRandomLower());//Random lowercase letter
 
 // Function that returns a random uppercase letter
 function getRandomUpper(){
 // Running the "getRandomLower" function to create a random letter and setting that value to the letter variable
     const letter = getRandomLower();
     // changing the random letter to uppercase and returning it from the function
-
     return letter.toUpperCase();
 }
+// Example of the getRandomUpper function
+console.log(getRandomUpper);//Random uppercase letter
 
-// Function that retrun a random number as Random Number as a string value)
+// Function that retrun a random number as Random Number as a string value
+
 function getRandomNumber(){
     const numbers = `0123456789`;
     // Returning a random number using a random index in the "numbers" string
@@ -36,6 +40,8 @@ function getRandomSymbol(){
 // Returning a random symbol using a random index in the "letters" string
     return symbols[randomIndex(symbols)];
 }
+// Example of the getRandomSynmbol function
+console.log(getRandomSymbol()); //Random symbol for the 'symbols' string
 
 // Object to store all the character generator functions
 
@@ -58,7 +64,7 @@ const generateEl = document.querySelector(`#generate`);
 
 // Function that accepts true or false values as well as a number as arguments
 // NOTE: The checkbox inputs and number/length input will determine the value/arguments entered into this function
-function generatePassword(lower, upper, number, symbol, length){
+// function generatePassword(lower, upper, number, symbol, length){
  
     console.log(lower, upper, number, symbol, length);
 // 1. CREATE THE PASSWORD VARIABLE
@@ -102,7 +108,7 @@ for (i =0; i<length; i += typesCount){
         generatedPassword += randomFunction[funcName]();
         console.log(generatedPassword);
     });
-}
+
 // 4. ADD THE GENERATED PASSWORD TO THE FINAL VARIABLE AND RETURN IT FROM THE FUNCTION
 // Removing extra characters if necessary (the above loop will create a password that may not match the length selected if that length is not a multiple of the number of options/checkboxes selected.)
 const finalPassword = generatedPassword.slice(0, length);
@@ -129,3 +135,38 @@ console.log(hasLower, hasUpper, hasNumber, hasSymbol, length);
 resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
 
+// COPY PASSWORD
+clipboardEl.addEventListener(`click`, () => {
+// Creating a textarea element which will be used to put the password inside of so that it can be selected/copied
+ const textarea = document.createElement(`textarea`)
+
+// Accessing the text/string value for the "result" span and setting it to the password variable 
+const password = resultEl.innerText;
+
+// If user clicks the clipboard while no password displayed the function will end and nothing will copied to the clipboard.
+if(password ===``){
+    alert (`Please generate a password first.`);
+    return; 
+}
+
+// Setting the value for the textarea to the password that is currently being displayed.
+ textarea.value = password;
+// Selecting the body element
+ const body = document.querySelector(`body`);
+
+// Adding the textarea to the webpage/document
+ body.append(textarea);
+
+//  Using the select method which selects (Focuses in on) an element.  This will highlight/select the value/password inside the textarea
+textarea.select();
+
+// Using execCommand to copy the selected value to the clipboard on the device the webpage is being viewed on.
+// NOTE: Some ExecCommand commands/arguments are not supported by all browsers.  However, copy is an execCommand command that all browsers support.
+document.execCommand(`copy`);
+
+// Removing the textarea element from the webpage/document
+textarea.remove();
+
+alert(`Password has been copied to the clipboard`);
+
+});
